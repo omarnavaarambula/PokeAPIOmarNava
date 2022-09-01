c9d904760a18ace2bbefa7970e79c0ebc01f57f3
omarnavaarambula <omarnavaarambula@gmail.com>
2022-08-31 22:08:18 -0500

    moves loading

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -32,31 +32,45 @@ console.log(pokeInfo)
     
     <article className={`card___center bg-${pokeInfo?.types[0].type.name}`} > 
     
-      <img src={pokeInfo?.sprites.other['official-artwork'].front_default} alt="" />
+      <img src={pokeInfo?.sprites.other['dream_world'].front_default} alt="" />
+       
+       
+       <div className={`poke__name bg-${pokeInfo?.types[0].type.name}`} > 
        <h1>Name: {name}</h1>
       <h2>Type: {pokeInfo?.types[0].type.name}</h2>
-
-
-  
       <ul>
-
         <li>{pokeInfo?.stats[0].stat.name}:{pokeInfo?.stats[0].base_stat} </li>
         <li>{pokeInfo?.stats[1].stat.name}:{pokeInfo?.stats[1].base_stat} </li>
         <li> {pokeInfo?.stats[5].stat.name}:{pokeInfo?.stats[5].base_stat} </li>
         <li> {pokeInfo?.stats[2].stat.name}:{pokeInfo?.stats[2].base_stat} </li>
         <li> {pokeInfo?.stats[3].stat.name}:{pokeInfo?.stats[3].base_stat} </li>
         <li> {pokeInfo?.stats[4].stat.name}:{pokeInfo?.stats[4].base_stat} </li>
-      
-      
+  
       </ul>
-   
+   </div>
+<div className={`movem__pok bg-${pokeInfo?.types[0].type.name}`}>
+<h2>
+        Movements
+      </h2>
+      <ul>
+        <li>  {pokeInfo?.moves[0].move.name} </li>
+        <li>  {pokeInfo?.moves[1].move.name} </li>
+        <li>  {pokeInfo?.moves[2].move.name} </li>
+        <li>  {pokeInfo?.moves[3].move.name} </li>
+        <li>  {pokeInfo?.moves[4].move.name} </li>
+
+
+      </ul>
+    </div>
+ 
+
       <button onClick={handleClick} className='home__btn'>Back All Pokemons  </button>
+   
+      
 
-
-
-
-
+   
     </article>
+ 
 
 </div>
   )
